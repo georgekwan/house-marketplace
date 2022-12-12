da3815f5248696a1b54dd33b200db48e14338585
George <gwmkwan@gmail.com>
2022-12-12 08:42:28 -0700

    feat: add offer and profile icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,14 @@ function Navbar() {
             <ExploreIcon fill="#2c2c2c" width="36px" height="36px" />
             <p>Explore</p>
           </li>
+          <li className="navbarListItem">
+            <OfferIcon fill="#2c2c2c" width="36px" height="36px" />
+            <p>Offer</p>
+          </li>
+          <li className="navbarListItem">
+            <PersonOutlineIcon fill="#2c2c2c" width="36px" height="36px" />
+            <p>Profile</p>
+          </li>
         </ul>
       </nav>
     </footer>
