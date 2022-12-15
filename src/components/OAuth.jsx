c9f1ab864fd656ee644ba25c61546c132731b68d
George <gwmkwan@gmail.com>
2022-12-15 08:42:49 -0700

    feat: add google login icon

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,5 +1,25 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import googleIcon from '../assets/svg/googleIcon.svg';
+
 function OAuth() {
-  return <div>OAuth</div>;
+  // Initialize navigate and location
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onGoogleClick = () => {};
+
+  return (
+    <div className="socialLogin">
+      <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with </p>
+      <button className="socialIconDiv" onClick={onGoogleClick}>
+        <img src={googleIcon} alt="google" />
+      </button>
+    </div>
+  );
 }
 
 export default OAuth;
