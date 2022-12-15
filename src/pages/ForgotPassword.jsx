0f7d625b3e1982773eb546c02e263b0898cc0d97
George <gwmkwan@gmail.com>
2022-12-14 21:20:36 -0700

    feat: create forgot password file

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+
 function ForgotPassword() {
   return (
     <div>
