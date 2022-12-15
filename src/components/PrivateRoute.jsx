76cdbbbc776f65af8504ad6c27f83b6d0caf31e0
George <gwmkwan@gmail.com>
2022-12-14 20:09:18 -0700

    feat: add check login status in PrivateRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,15 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStatus } from '../hooks/useAuthStatus';
 
 const PrivateRoute = () => {
-  const loggedIn = false;
+  // Destructure useAuthStatus into loggedIn and checkingStatus
+  const { loggedIn, checkingStatus } = useAuthStatus();
 
+  if (checkingStatus) {
+    return <h3>Loading...</h3>;
+  }
+
+  // If user is not logged in it would forward to sign in page
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
