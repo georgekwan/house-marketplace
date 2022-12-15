7356e4dd48f437901f290ae26ced7fc2f593cefd
George <gwmkwan@gmail.com>
2022-12-14 18:51:55 -0700

    feat: set class depend on the state

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
-import { getAuth } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { updateDoc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Profile() {
+  // initialize the Auth library
   const auth = getAuth();
+  // Detail get update whenever submit button has been pres s
+  const [changeDetails, setChangeDetails] = useState(false);
   // It uses the pre-filled the user's name and email then store it in formData
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -20,6 +25,10 @@ function Profile() {
     auth.signOut();
     navigate('/');
   };
+
+  const onSubmit = () => {
+    console.log(123);
+  };
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -28,6 +37,28 @@ function Profile() {
           Logout
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails(prevState => !prevState);
+            }}>
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
