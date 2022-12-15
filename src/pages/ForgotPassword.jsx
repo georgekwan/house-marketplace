129fb56f4eca45bb150df0448ecfa5bbfdcaaa7a
George <gwmkwan@gmail.com>
2022-12-14 21:51:50 -0700

    feat: add send email link

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -6,9 +6,16 @@ import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRig
 
 function ForgotPassword() {
   const [email, setEmail] = useState('');
-  const onChange = e => {};
-  const onSubmit = e => {
+  const onChange = e => setEmail(e.target.value);
+  const onSubmit = async e => {
     e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success('Email was sent');
+    } catch (error) {
+      toast.error('Could not send reset email');
+    }
   };
   return (
     <div className="pageContainer">
