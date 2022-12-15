f711c4c1515f162528d9d0f340d91f4fe0a7bac3
George <gwmkwan@gmail.com>
2022-12-14 21:32:21 -0700

    feat: add email input

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,11 +2,31 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 import { toast } from 'react-toastify';
+import { ReactComponentElement as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 
 function ForgotPassword() {
+  const [email, setEmail] = useState('');
+  const onChange = e => {};
+  const onSubmit = e => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Forgot Password</h1>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+        </form>
+      </main>
     </div>
   );
 }
