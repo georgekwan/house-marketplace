38a8fbf6af003c01839d214136e315071a956513
George <gwmkwan@gmail.com>
2022-12-17 22:13:11 -0700

    feat: add show bedroom(s)

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -28,7 +28,14 @@ function ListingItem({ listing, id }) {
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
             {listing.type === 'rent' && ' / Month'}
           </p>
-          <div className="categoryListingInfoDiv"></div>
+          <div className="categoryListingInfoDiv">
+            <img src={bedIcon} alt="bed" />
+            <p className="categoryListingInfoText">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} Bedrooms`
+                : '1 Bedroom'}
+            </p>
+          </div>
         </div>
       </Link>
     </li>
