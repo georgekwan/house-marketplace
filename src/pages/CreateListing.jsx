06d8ea17a135fc62b5b67377c998ddf02ec44eb8
George <gwmkwan@gmail.com>
2022-12-18 11:05:41 -0700

    feat: create form for input listing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+
 function CreateListing() {
+  const [formData, setFormData] = useState({
+    type: 'rent',
+    name: '',
+    bedroom: 1,
+    bathroom: 1,
+    parking: false,
+    furnished: false,
+    address: '',
+    offer: false,
+    regularPrice: 0,
+    discountPrice: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  });
   return <div>Create</div>;
 }
 
