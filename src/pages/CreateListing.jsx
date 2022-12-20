768202e526d8bde70b466705397b5d9dca93eaa7
George <gwmkwan@gmail.com>
2022-12-19 21:24:47 -0700

    feat: add image upload

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -275,6 +275,19 @@ function CreateListing() {
             />
           </>
         )}
+
+        <label className="formLabel">Images</label>
+        <p className="imagesInfo">The first image will be the cover (max 6).</p>
+        <input
+          className="formInputFile"
+          type="file"
+          id="images"
+          onChange={onMutate}
+          max="6"
+          accept=".jpg,.png,.jpeg"
+          multiple
+          required
+        />
       </main>
     </div>
   );
