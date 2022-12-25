555167bffaeaa2e45e8a065e5c7c3c17f5c8f344
George <gwmkwan@gmail.com>
2022-12-24 19:21:48 -0700

    feat: add Listing route

--- src/App.js
+++ src/App.js
@@ -29,7 +29,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
-
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
