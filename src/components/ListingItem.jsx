6248aed18b3a4fa27ba61138fd3d277a455da118
George <gwmkwan@gmail.com>
2022-12-24 18:04:45 -0700

    fix: bug fix on categoryListing

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -10,13 +10,14 @@ function ListingItem({ listing, id, onDelete }) {
         to={`/category/${listing.type}/${id}`}
         className="categoryListingLink">
         <img
-          src={listing.imageUrls[0]}
+          src={listing.imgUrls[0]}
           alt={listing.name}
           className="categoryListingImg"
         />
         <div className="categoryListingDetails">
           <p className="categoryListingLocation">{listing.location}</p>
           <p className="categoryListingName">{listing.name}</p>
+
           <p className="categoryListingPrice">
             $
             {listing.offer
