0634033263dfe404c9fd5b090c799a10a1a049c4
George <gwmkwan@gmail.com>
2022-12-24 19:09:34 -0700

    feat: create Contact page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -28,10 +29,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route
-            path="/category/:categoryName/:listingId"
-            element={<Listing />}
-          />
+
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
