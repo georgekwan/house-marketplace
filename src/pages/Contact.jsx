7e5bbc682adbb04d4c355adc7d3dea25e8822aee
George <gwmkwan@gmail.com>
2022-12-25 09:51:34 -0700

    feat: add swipeable listing image

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify';
 function Contact() {
   const [message, setMessage] = useState('');
   const [landlord, setLandlord] = useState(null);
+  // eslint-disable-next-line
   const [searchParams, setSearchParams] = useSearchParams();
 
   const params = useParams();
@@ -21,6 +22,7 @@ function Contact() {
         toast.error('Could not get landlord data');
       }
     };
+
     getLandlord();
   }, [params.landlordId]);
 
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
@@ -36,7 +39,25 @@ function Listing() {
 
   return (
     <main>
-      {/* SLIDER */}
+      <Swiper
+        slidesPerView={1}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        pagination={true}
+        navigation
+        scrollbar={{ dragable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) 
+                            center no-repeat`,
+                backgroundSize: 'cover',
+                minHeight: '20rem',
+              }}
+              className="swiperSlideDiv"></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
       <div
         className="shareIconDiv"
         onClick={() => {
