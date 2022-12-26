ac0795be14449ed8beba19f78ca2fe71a8ff40af
George <gwmkwan@gmail.com>
2022-12-26 12:06:51 -0700

    refactor: remove firebase async

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -13,7 +13,6 @@ import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
 import ListingItem from '../components/ListingItem';
-import { async } from '@firebase/util';
 
 function Category() {
   const [listings, setListings] = useState(null);
