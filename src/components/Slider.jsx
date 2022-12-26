c35015cc24dcb7f1a053dd9ecbcf82599b120fd7
George <gwmkwan@gmail.com>
2022-12-25 19:28:36 -0700

    feat: add pricing to listing slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -61,6 +61,10 @@ function Slider() {
                 }}
                 className="swiperSlideDiv">
                 <p className="swiperSlideText">{data.name}</p>
+                <p className="swiperSlidePrice">
+                  ${data.discountedPrice ?? data.regularPrice}{' '}
+                  {data.type === 'rent' && '/ month'}
+                </p>
               </div>
             </SwiperSlide>
           ))}
